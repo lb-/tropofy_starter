bd3c341ab97e06c72bfbf0f19c2b090bf1fc0a7b
LB <mail@lb.ee>
2017-11-20 14:31:55 +0800

    add kml widget

--- te_starter/starter.py
+++ te_starter/starter.py
@@ -1,11 +1,13 @@
 from collections import OrderedDict
 
+from simplekml import Kml
+
 from sqlalchemy.schema import Column
 from sqlalchemy.types import Float, Text
 
 from tropofy.app import AppWithDataSets, Step, StepGroup
 from tropofy.database.tropofy_orm import DataSetMixin
-from tropofy.widgets import SimpleGrid
+from tropofy.widgets import KMLMap, SimpleGrid
 
 
 class Store(DataSetMixin):
@@ -14,19 +16,34 @@ class Store(DataSetMixin):
     longitude = Column(Float)
 
 
+class MyKMLMap(KMLMap):
+    def get_kml(self, app_session):
+        kml = Kml()
+        stores = app_session.data_set.query(Store).all()
+        for store in stores:
+            kml.newpoint(
+                name=store.name,
+                coords=[(store.longitude, store.latitude)]
+            )
+        return kml.kml()
+
+
 class MyFirstApp(AppWithDataSets):
     def get_name(self):
-        return "My First App"
+        return 'Starter App'
 
     def get_gui(self):
-        return OrderedDict([
-            ('stores', StepGroup(
+        return [
+            StepGroup(
                 name='Stores',
-                steps=OrderedDict([
-                    ('stores', Step(
-                        name='Stores',
-                        widgets=[SimpleGrid(Store)]
-                    ))
-                ]),
-            )),
-        ])
+                steps=[
+                    Step(name='stored', widgets=[SimpleGrid(Store)])
+                ]
+            ),
+            StepGroup(
+                name='Map',
+                steps=[
+                    Step(name='Map', widgets=[MyKMLMap()])
+                ]
+            )
+        ]
